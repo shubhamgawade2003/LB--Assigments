82b3e916aad70caae2416d33d2b8af8055ac4eed
shubhamgawade2003 <135878940+shubhamgawade2003@users.noreply.github.com>
2025-07-05 01:24:25 +0530

    Add files via upload

---
+++ Assignment 16/Program80.c
@@ -0,0 +1,59 @@
+//////////////////////////////////////////////////////////////////////////////////////////////
+// problem statement :  Accept number of rows and columns from user and display below pattern
+//
+//
+// Input  : iColunm =4 iRow = 4
+// output : 1 2 3 4
+//            2 3 4
+//              3 4
+//                4
+//
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int iCntCol = 0;
+    int iCntRow = 0;
+    if (iRow != iCol)
+    {
+        printf("Invalid input");
+        return;
+    }
+
+    for (iCntRow = 1; iCntRow <= iRow; iCntRow++)
+    {
+
+        for (iCntCol = 1; iCntCol <= iCol; iCntCol++)
+        {
+            if (iCntRow > iCntCol)
+            {
+                printf("\t");
+            }
+            else
+            {
+                printf("%d\t", iCntCol);
+            }
+        }
+
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter the number of Rows:\n");
+    scanf("%d", &iValue1);
+
+    printf("Enter the number of Columns:\n");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
